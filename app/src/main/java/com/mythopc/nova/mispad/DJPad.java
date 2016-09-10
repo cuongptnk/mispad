4eb4a9c9ab6287a9fe92d44b0650bc6dd1bc1d18
cuongptnk <nguyenphucuong250690@gmail.com>
2016-09-10 14:32:38 -0300

    MusicPlayerView
    Play and Pause with MusicPlayerView

--- app/src/main/java/com/mythopc/nova/mispad/DJPad.java
+++ app/src/main/java/com/mythopc/nova/mispad/DJPad.java
@@ -1,6 +1,7 @@
 package com.mythopc.nova.mispad;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 
 import java.io.IOException;
 
+import co.mobiwise.library.MusicPlayerView;
+
 public class DJPad extends AppCompatActivity implements View.OnClickListener {
 
     Button song1Start, song1Stop, song2Start, song2Stop;
@@ -20,6 +23,8 @@ public class DJPad extends AppCompatActivity implements View.OnClickListener {
     SoundPool sp;
     MediaPlayer mediaPlayer1;
     MediaPlayer mediaPlayer2;
+    MusicPlayerView mpv1;
+    MusicPlayerView mpv2;
 
 
 
@@ -30,25 +35,44 @@ public class DJPad extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_djpad);
 
-        song1Start = (Button)findViewById(R.id.song1buttonstart);
-        song1Stop  = (Button)findViewById(R.id.song1buttonstop);
-        song2Start = (Button)findViewById(R.id.song2buttonstart);
-        song2Stop = (Button)findViewById(R.id.song2buttonstop);
 
-        song1 = (TextView)findViewById(R.id.song1);
-        song2 = (TextView)findViewById(R.id.song2);
+
 
         Intent i = getIntent();
         r1 = i.getStringExtra("filename1");
         r2 = i.getStringExtra("filename2");
 
-        song1.setText(r1);
-        song2.setText(r2);
+        mediaPlayer1 = new MediaPlayer();
+        mediaPlayer1.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer1.setDataSource(r1);
+            mediaPlayer1.prepare();
+        } catch (IOException e) {
+        }
 
-        song1Start.setOnClickListener(this);
-        song2Start.setOnClickListener(this);
-        song1Stop.setOnClickListener(this);
-        song2Stop.setOnClickListener(this);
+        mediaPlayer2 = new MediaPlayer();
+        mediaPlayer2.setAudioStreamType(AudioManager.STREAM_RING);
+        try {
+            mediaPlayer2.setDataSource(r2);
+            mediaPlayer2.prepare();
+        }
+        catch (IOException e) {
+        }
+
+
+        mpv1 = (MusicPlayerView) findViewById(R.id.mpv1);
+        mpv2 = (MusicPlayerView) findViewById(R.id.mpv2);
+
+        mpv1.setTimeColor(Color.BLUE);
+        mpv1.setCoverDrawable(R.drawable.mycover);
+
+
+        mpv2.setTimeColor(Color.BLUE);
+        mpv2.setCoverDrawable(R.drawable.cover2);
+
+
+        mpv1.setOnClickListener(this);
+        mpv2.setOnClickListener(this);
 
 
 
@@ -59,39 +83,28 @@ public class DJPad extends AppCompatActivity implements View.OnClickListener {
     }
 
     public void onClick(View view) {
-        if(view == song1Start) {
-            mediaPlayer1 = new MediaPlayer();
-            mediaPlayer1.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            try {
-                mediaPlayer1.setDataSource(r1);
-                mediaPlayer1.prepare();
-            } catch (IOException e) {
-
+        if (view == mpv1) {
+            if (mpv1.isRotating()) {
+                mediaPlayer1.pause();
+                mpv1.stop();
+            } else {
+                mediaPlayer1.start();
+                mpv1.start();
             }
-            mediaPlayer1.start();
         }
 
-
-        if(view == song2Start) {
-            mediaPlayer2 = new MediaPlayer();
-            mediaPlayer2.setAudioStreamType(AudioManager.STREAM_RING);
-            try {
-                mediaPlayer2.setDataSource(r2);
-                mediaPlayer2.prepare();
+        if (view == mpv2) {
+            if (mpv2.isRotating()) {
+                mediaPlayer2.pause();
+                mpv2.stop();
+            } else {
+                mediaPlayer2.start();
+                mpv2.start();
             }
-            catch (IOException e) {
-
-            }
-            mediaPlayer2.start();
         }
 
-        if(view == song1Stop) {
-            mediaPlayer1.stop();
-        }
 
-        if(view == song2Stop) {
-            mediaPlayer2.stop();
-        }
+
 
     }
 
